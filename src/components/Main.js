c0328455fe81fdf956797879d41039322a190735
Vinicius Silva Teixeira <viniciusst@gmail.com>
2025-01-28 10:48:48 -0500

    Add the first components

---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+function Main() {
+  return (
+    <>
+      <div className="card">
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </div>
+    </>
+  );
+}
+
+export default Main;
